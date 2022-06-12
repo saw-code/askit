42712da993493f200839846efdfa35b03dba6cca
saw-code <saw.coding@gmail.com>
2022-06-12 18:50:46 +0300

    Lesson 6

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,6 @@
 class AnswersController < ApplicationController
   before_action :set_question!
+  before_action :set_answer!, except: :create
 
   def create
     @answer = @question.answers.build(answer_params)
@@ -7,20 +8,26 @@ class AnswersController < ApplicationController
       flash[:success] = 'Answer created!'
       redirect_to question_path(@question)
     else
-      @answers = Answer.order(created_at: :desc)
+      @answers = @question.answers.order(created_at: :desc)
       render 'questions/show'
+    end
+  end
+
+  def update
+    if @answer.update(answer_params)
+      flash[:success] = 'Answer updated!'
+      redirect_to question_path(@question)
+    else
+      render :edit
     end
   end
 
   def edit
   end
 
-  def update
-  end
 
   def destroy
-    answer = @question.answers.find(params[:id])
-    answer.destroy
+    @answer.destroy
     flash[:success] = 'Answer deleted!'
     redirect_to question_path(@question)
   end
@@ -33,6 +40,10 @@ class AnswersController < ApplicationController
 
   def set_question!
     @question = Question.find(params[:question_id])
+  end
+
+  def set_answer!
+    @answer = @question.answers.find(params[:id])
   end
 end
 
